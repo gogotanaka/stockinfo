3f1283b6ec756ca67c0d70215aff84d4ed5212fd
Tanaka Kazuki <tanaka@172-30-2-46.xfw2.hc.keio.ac.jp>
2013-07-06 12:37:52 +0900

    detail

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -3,7 +3,7 @@ class StockController < ApplicationController
   def show
     id = params[:id].to_i
     span = params[:span] ? params[:span].to_i : 100
-    @stocks = Stock.order("updated_at DESC").all[id*span..id*span+(span-1)]
+    @stocks = Stock.order("code DESC").all[id*span..id*span+(span-1)]
   end
 
   def info
